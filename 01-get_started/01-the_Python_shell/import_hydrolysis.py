3587d4f099d31b07b1a668c1f7c5106251f86fd8
krother <krother@localhost>
2013-08-19 13:41:21 +0200

    added scripts for course module 1

---
+++ 01-get_started/01-the_Python_shell/import_hydrolysis.py
@@ -0,0 +1,17 @@
+'''
+Import a module from the same folder
+
+-----------------------------------------------------------
+(c) 2013 Allegra Via and Kristian Rother
+    krother@academis.eu
+
+    Licensed under the conditions of the Python License
+
+    This code appears in section 1.4.2 of the book
+    "Managing Biological Data with Python".
+-----------------------------------------------------------
+'''
+
+from hydrolysis import ATP
+
+print ATP
