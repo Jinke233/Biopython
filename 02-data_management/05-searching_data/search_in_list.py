178d37e523add0de6f5e8c970a539daf0ba31868
krother <krother@localhost>
2013-08-20 11:54:23 +0200

    added code examples for module 5.

--- 02-data_management/05-searching_data/search_in_list.py
+++ 02-data_management/05-searching_data/search_in_list.py
@@ -1,14 +1,17 @@
 '''
 
-Example for loop over a string.
+Search for non-header lines in a FASTA file.
 
 -----------------------------------------------------------
 (c) 2013 Allegra Via and Kristian Rother
     Licensed under the conditions of the Python License
 
-    This code appears in section 2.3.5 of the book
+    This code appears in section 5.3.4 of the book
     "Managing Biological Data with Python".
 -----------------------------------------------------------
 '''
-for character in 'hemoglobin':
-    print character,
+
+rna = ''
+for line in open('A06662-RNA.fasta'):
+    if not line.startswith('>'): 
+        rna = rna + line.strip()
